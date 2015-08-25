db022ff3f69eb90fcdf5bc2e59a3e926a45e14c6
bugTrap <potatomoto227@gmail.com>
2015-08-25 09:31:52 +0200

    Turn structure

--- scriptold.js
+++ scriptold.js
@@ -1,188 +1,242 @@
-var BOARD = {
-	'tileSize': 26,
-	'impassableTiles': [
-		[0, 0],
-		[0, 1],
-		[0, 9],
-		[0, 10],
-		[0, 11],
-		[0, 12],
-		[1, 0],
-		[1, 11],
-		[1, 12],
-		[2, 11],
-		[2, 12],
-		[3, 12],
-		[4, 12],
-		[5, 6],
-		[5, 7],
-		[6, 6],
-		[6, 7],
-		[7, 7],
-		[8, 7],
-		[9, 0],
-		[9, 7],
-		[10, 0],
-		[10, 12],
-		[11, 0],
-		[11, 12],
-		[12, 11],
-		[12, 12],
-		[13, 11],
-		[13, 12],
-		[14, 10],
-		[14, 11],
-		[14, 12],
-		[15, 10],
-		[15, 11],
-		[15, 12],
-		[16, 9],
-		[16, 10],
-		[16, 11],
-		[16, 12],
-		[17, 0],
-		[17, 8],
-		[17, 9],
-		[17, 10],
-		[17, 11],
-		[17, 12]
-],
-	'oasisTiles': [
-		[ 16, 2 ],
-		[ 12, 6 ],
-		[ 9, 2 ],
-		[ 7, 10 ],
-		[ 3, 2 ],
-		[ 1, 7 ]
-],
-	'waterholeTiles':[
-		[ 17, 5 ],
-		[ 16, 0 ],
-		[ 16, 6 ],
-		[ 15, 4 ],
-		[ 14, 1 ],
-		[ 14, 6 ],
-		[ 14, 8 ],
-		[ 13, 4 ],
-		[ 12, 9 ],
-		[ 11, 1 ],
-		[ 11, 3 ],
-		[ 11, 5 ],
-		[ 11, 8 ],
-		[ 10, 11 ],
-		[ 9, 8 ],
-		[ 9, 10 ],
-		[ 8, 4 ],
-		[ 7, 1 ],
-		[ 7, 3 ],
-		[ 6, 8 ],
-		[ 6, 11 ],
-		[ 5, 2 ],
-		[ 5, 4 ],
-		[ 4, 5 ],
-		[ 4, 9 ],
-		[ 4, 11 ],
-		[ 3, 0 ],
-		[ 3, 8 ],
-		[ 2, 4 ],
-		[ 1, 3 ],
-		[ 1, 9 ],
-		[ 0, 5 ],
-]};
+// MAP width = 0..17 (18)
+// MAP height = 0..12 (13)
 
-var GAME = {
-	'board': new Board(18, 13),
-	'turn': new TurnState(0, 1)
+var IMPASSABLE_TILES = [
+	[0, 0],
+	[0, 1],
+	[0, 9],
+	[0, 10 ],
+	[0, 11 ],
+	[0, 12],
+	[1, 0],
+	[1, 11],
+	[1, 12],
+	[2, 11],
+	[2, 12],
+	[3, 12],
+	[4, 12],
+	[5, 6],
+	[5, 7],
+	[6, 6],
+	[6, 7],
+	[7, 7],
+	[8, 7],
+	[9, 0],
+	[9, 7],
+	[10, 0],
+	[10, 12],
+	[11, 0],
+	[11, 12],
+	[12, 11],
+	[12, 12],
+	[13, 11],
+	[13, 12],
+	[14, 10],
+	[14, 11],
+	[14, 12],
+	[15, 10],
+	[15, 11],
+	[15, 12],
+	[16, 9],
+	[16, 10],
+	[16, 11],
+	[16, 12],
+	[17, 0],
+	[17, 8],
+	[17, 9],
+	[17, 10],
+	[17, 11],
+	[17, 12]
+];
+
+var OASIS_TILES = [
+	[ 16, 2 ],
+	[ 12, 6 ],
+	[ 9, 2 ],
+	[ 7, 10 ],
+	[ 3, 2 ],
+	[ 1, 7 ]
+];
+
+var WATERHOLES_TILES = [
+	[ 17, 5 ],
+	[ 16, 0 ],
+	[ 16, 6 ],
+	[ 15, 4 ],
+	[ 14, 1 ],
+	[ 14, 6 ],
+	[ 14, 8 ],
+	[ 13, 4 ],
+	[ 12, 9 ],
+	[ 11, 1 ],
+	[ 11, 3 ],
+	[ 11, 5 ],
+	[ 11, 8 ],
+	[ 10, 11 ],
+	[ 9, 8 ],
+	[ 9, 10 ],
+	[ 8, 4 ],
+	[ 7, 1 ],
+	[ 7, 3 ],
+	[ 6, 8 ],
+	[ 6, 11 ],
+	[ 5, 2 ],
+	[ 5, 4 ],
+	[ 4, 5 ],
+	[ 4, 9 ],
+	[ 4, 11 ],
+	[ 3, 0 ],
+	[ 3, 8 ],
+	[ 2, 4 ],
+	[ 1, 3 ],
+	[ 1, 9 ],
+	[ 0, 5 ],
+];
+
+
+var FLAGS = {
+	'impassable': false,
+	'oasis': false,
+	'waterhole': false,
+	'LeadersPlaced': []
 };
+
+var TILESIZE = 26;
+var BOARD = new Board(18, 13);
 
 var UI = {
 	'selectedTile': null
-};
+}
 
 
 
 // #########################################################################
 // ##																	  ##
-// ##						   	GAME.board GENERATION						  ##
+// ##						   	BOARD GENERATION						  ##
 // ##																	  ##
 // #########################################################################
 
-function prepareBoard () {
 
-	updateUI();
-	GAME.board.setImpassable();
-	var splice = GAME.board.setOasis()[0];
-	GAME.board.setWaterholes(splice);
-	drawBoardState();
+function setImpassable () {
+	switch (FLAGS.impassable) {
 
-	document.querySelector('button').addEventListener('click', placeCamel);
-	document.querySelector('button + button').addEventListener('click', GAME.board.clearBoard.bind(GAME.board));
-	document.querySelector('button + button + button').addEventListener('click', newTurn);
+		case false:
 
-}
+			for (var i = 0; i < IMPASSABLE_TILES.length; i++) {
+				BOARD.getTile(IMPASSABLE_TILES[i]).impassable = true;
+				// console.log(BOARD.getTile(IMPASSABLE_TILES[i]));
+			};
 
-Board.prototype.setImpassable = function () {
-	for (var i = 0; i < BOARD.impassableTiles.length; i++) {
-		GAME.board.getTile(BOARD.impassableTiles[i]).impassable = true;
-	};
-}
+			FLAGS.impassable = true;
+			break;
 
-Board.prototype.setOasis = function () {
-
-	var rnd = Math.floor(Math.random()*6);
-	var loc = BOARD.oasisTiles.slice(0);
-	var splice = loc.splice(rnd, 1);
-	for (var i = 0; i < loc.length; i++) {
-		GAME.board.getTile(loc[i]).oasis = true;
-	};
-	return splice;
-}
-
-Board.prototype.setWaterholes = function (arr) {
-	//Takes the last remaining free Oasis tile as argument, and adds Waterholes
-	var holes = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, ];
-	var loc = BOARD.waterholeTiles.slice(0);
-	loc.push(arr);
-
-	while (loc.length > 0) {
-		var rnd = Math.floor(Math.random() * holes.length);
-		var tile = loc.splice(0, 1)[0];
-		GAME.board.getTile(tile).waterhole = holes.splice(rnd, 1)[0];
+		case true:
+			break;
 	}
 }
 
+function setOasis () {
 
-// CREATORS
+	switch (FLAGS.oasis) {
 
-function Camel (owner, color) {
-	this.owner = owner;
-	this.color = false;
-	this.leader = false;
+		case false:
+			//Make a copy of the list of Oasis tiles with slice 0, then splice away one random element,
+			//then set the corresponding Tile objects Oasis flag to true, then set the general oasis flag to true
+
+			var rnd = Math.floor(Math.random()*6);
+			var loc = OASIS_TILES.slice(0);
+			var splice = loc.splice(rnd, 1);
+
+			for (var i = 0; i < loc.length; i++) {
+				BOARD.getTile(loc[i]).oasis = true;
+				// console.log(BOARD.getTile(loc[i]));
+
+			};
+			// console.log(splice);
+			FLAGS.oasis = true;
+			return splice;
+			break;
+
+		case true:
+			throw 'Oasis already generated';
+			break;
+	}
 }
+
+function setWaterholes (arr) {
+
+	//Takes the last remaining free Oasis tile as argument, and adds Waterholes
+
+	switch (FLAGS.waterhole) {
+
+		case false:
+
+			var holes = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, ];
+			var loc = WATERHOLES_TILES.slice(0);
+			loc.push(arr);
+			// debugger;
+
+			while (loc.length > 0) {
+				var rnd = Math.floor(Math.random() * holes.length);
+				var tile = loc.splice(0, 1)[0];
+				BOARD.getTile(tile).waterhole = holes.splice(rnd, 1)[0];
+				// console.log(holes);
+			}
+			FLAGS.waterhole = true;
+			break;
+
+		case true:
+			break;
+	}
+}
+
+function prepareBoard () {
+	// BOARD.printBoard();
+	setImpassable();
+	var splice = setOasis()[0];
+	setWaterholes(splice);
+	drawBoardState();
+
+	document.querySelector('button').addEventListener('click', playPiece);
+	document.querySelector('button + button').addEventListener('click', removePiece);
+	document.querySelector('button + button + button').addEventListener('click', placeLeader);
+
+	// draw_impassable('#333');
+	// draw_oasis('#4F4');
+	// draw_waterholes();
+}
+
+
 
 function Tile (arr) {
 
 	this.impassable = false;
 	this.oasis = false;
 	this.waterhole = false;
-	this.camel = null;
-	this.selected = false;
+	this.piece = {
+		'exists': false,
+		'color': false,
+	}
 	
-	var x = arr[0] * BOARD.tileSize * 1.75 + BOARD.tileSize;
+	var x = arr[0] * TILESIZE * 1.75 + TILESIZE;
 	if (arr[0] % 2 > 0) {
-		var y = arr[1] * BOARD.tileSize * 2 + BOARD.tileSize * 2;
+		var y = arr[1] * TILESIZE * 2 + TILESIZE * 2;
 	} else {
-		var y = arr[1] * BOARD.tileSize * 2 + BOARD.tileSize;
+		var y = arr[1] * TILESIZE * 2 + TILESIZE;
 	}
 
 	this.center = [x, y, arr[0], arr[1]];
+
+	this.selected = false;
 }
 
 function Board (width, height) {
-	
+	// Transform an array of rows in a Board object made of list of Tile objects
+
 	this.board = [];
 	this.width = width;
 	this.height = height;
+	this.tilesize = TILESIZE;
 
 	for (var i = 0; i < width; i++) {
 		var column = [];
@@ -191,15 +245,18 @@ function Board (width, height) {
 			column.push(new Tile(coor));
 		};
 		this.board.push(column);
+		// console.log(this.board);
 	};
 
 	this.getTile = function (arr) {
+		// console.log(arr, arr[0], arr[1]);
 		// Takes an array of coordinates and returns the corresponding Tile object
 		if (arr[0] < 0 || arr[0] >= this.width || arr[1] < 0 || arr[1] >= this.height) {
 			return null;
 		} else {
 			return this.board[arr[0]][arr[1]];
 		}
+		
 	}
 
 	this.getAdjacent = function (arr) {
@@ -229,16 +286,56 @@ function Board (width, height) {
 					[x+1,y],
 					[x+1,y+1],
 				];
+				console.log('uneven');
 				break;
 		}
 
 		for (var i = 0; i < tiles_to_find.length; i++) {
 			if (tiles_to_find[i][0] < 0 || tiles_to_find[i][0] >= this.width || tiles_to_find[i][1] < 0 || tiles_to_find[i][1] >= this.height) {
 			} else {
-				adj_tiles.push(GAME.board.board[tiles_to_find[i][0]][tiles_to_find[i][1]])
+				adj_tiles.push(BOARD.board[tiles_to_find[i][0]][tiles_to_find[i][1]])
 			}
 		};
 	return adj_tiles;
+	}
+
+	this.getImpassable = function () {
+		var tiles = [];
+		var callback = function (tile, index) {
+			if (tile.impassable === true) {
+				tiles.push([i, index]);
+			}
+		}
+		for (var i = 0; i < this.board.length; i++) {
+			this.board[i].map(callback);
+		};
+		return tiles;
+	}
+
+	this.getWaterholes = function () {
+		var tiles = [];
+		var callback = function (tile, index) {
+			if (typeof tile.waterhole === 'number') {
+				tiles.push([i, index, tile.waterhole]);
+			}
+		}
+		for (var i = 0; i < this.board.length; i++) {
+			this.board[i].map(callback);
+		};
+		return tiles;
+	}
+
+	this.getOasis = function () {
+		var tiles = [];
+		var callback = function (tile, index) {
+			if (tile.oasis === true) {
+				tiles.push([i, index]);
+			}
+		}
+		for (var i = 0; i < this.board.length; i++) {
+			this.board[i].map(callback);
+		};
+		return tiles;
 	}
 
 	this.getCoorList = function () {
@@ -258,23 +355,8 @@ function Board (width, height) {
 			};
 		};
 	}
-
-	this.clearBoard = function () {
-		for (var i = 0; i < this.board.length; i++) {
-			for (var j = 0; j < this.board[i].length; j++) {
-				this.board[i][j].camel = null;
-			};
-		};
-		drawBoardState();
-	}
 }
 
-
-function TurnState (player, turn) {
-	this.turn = turn;
-	this.activeplayer = player;
-	this.camels = 0;
-}
 
 // #########################################################################
 // ##																	  ##
@@ -287,11 +369,12 @@ function drawBoardState () {
 	var canvas = document.getElementById('board');
 	var ctx = canvas.getContext('2d');
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	for (var i = 0; i < GAME.board.board.length; i++) {
-		for (var j = 0; j < GAME.board.board[i].length; j++) {
-			
-			var tile = GAME.board.board[i][j]
-			var circrad = BOARD.tileSize
+
+	for (var i = 0; i < BOARD.board.length; i++) {
+		for (var j = 0; j < BOARD.board[i].length; j++) {
+
+			var tile = BOARD.board[i][j]
+			var circrad = BOARD.tilesize
 			var x = tile.center[2] * circrad * 1.75 + circrad;
 			var y = tile.center[3] * circrad * 2 + circrad * ((i % 2 > 0) ? 2 : 1);
 
@@ -310,9 +393,9 @@ function drawBoardState () {
 				drawEmptyHex(x, y, circrad, '#FA8');
 			}
 
-			if (tile.camel != null) {
+			if (tile.piece.exists === true) {
 				var color = '#FFF'
-				switch	(tile.camel.color) {
+				switch	(tile.piece.color) {
 					case 'Mint':
 						color = '#BEF6E9';
 						break;
@@ -331,7 +414,7 @@ function drawBoardState () {
 					default:
 						break;
 				}
-				drawCamel(x, y, circrad, color);
+				drawPiece(x, y, circrad, color);
 			}
 
 		};
@@ -393,7 +476,7 @@ function drawEmptyHex (x, y, cirumradius, fill) {
 	ctx.fill(path);
 }
 
-function drawCamel (x, y, cirumradius, fill) {
+function drawPiece (x, y, cirumradius, fill) {
 
 	var canvas = document.getElementById('board');
 	var ctx = canvas.getContext('2d');
@@ -419,13 +502,14 @@ function drawNumber (tile) {
 	var x = tile.center[2]
 	var y = tile.center[3]
 
-	var circumradius = BOARD.tileSize * 0.6;
+
+	var circumradius = BOARD.tilesize * 0.6;
 
 	var canvas = document.getElementById('board');
     var ctx = canvas.getContext('2d');
 
-	var center_x = x * 1.75 * BOARD.tileSize + BOARD.tileSize;
-	var center_y = y * 2 * BOARD.tileSize + BOARD.tileSize * ((x % 2 > 0) ? 2 : 1);
+	var center_x = x * 1.75 * BOARD.tilesize + BOARD.tilesize;
+	var center_y = y * 2 * BOARD.tilesize + BOARD.tilesize * ((x % 2 > 0) ? 2 : 1);
 
 	var origin_x = (Math.cos(Math.PI * 6 / 8) * circumradius) + center_x;
 	var origin_y = (Math.sin(Math.PI * 3 / 8) * circumradius) + center_y;
@@ -450,7 +534,7 @@ function drawNumber (tile) {
 
 function isTile (arr, arr1) {
 	//Tells if the first point belongs to the tile centered in the second point
-	if (PITAGORA(arr, arr1) <= BOARD.tileSize) {
+	if (PITAGORA(arr, arr1) <= BOARD.tilesize) {
 		return true;
 	} else {
 		return false;
@@ -460,7 +544,7 @@ function isTile (arr, arr1) {
 function closestTile (arr) {
 	//Finds the closest tile on the board given an array containing two coordinates. Returns their board coordinates
 	var distances = [];
-	var coor_list = GAME.board.getCoorList();
+	var coor_list = BOARD.getCoorList();
 	for (var i = 0; i < coor_list.length; i++) {
 		distances.push(PITAGORA(arr, coor_list[i]));
 	};
@@ -497,14 +581,6 @@ function PITAGORA (arr, arr2) {
 }
 
 
-// UI RELATED
-
-function updateUI () {
-	document.querySelector('div [name=player]').textContent = 'Player ' + (parseInt(GAME.turn.activeplayer) + 1).toString();
-	document.querySelector('div [name=turn]').textContent = 'Turn ' + GAME.turn.turn;
-	document.querySelector('div [name=camels]').textContent = Math.abs(GAME.turn.camels - 2) + ' Camels';
-}
-
 // #########################################################################
 // ##																	  ##
 // ##						   	EVENT HANDLERS							  ##
@@ -516,17 +592,17 @@ function testClick (ev) {
 
 	console.log('clickCoor', ev.layerX, ev.layerY);
 
-	var coordinates = [ev.layerX, ev.layerY];
-	var tile = closestTile(coordinates);
+	var coor = [ev.layerX, ev.layerY];
+	var tile = closestTile(coor);
 
 	if (tile[2] === true) {
 
-		GAME.board.resetClicked();
+		BOARD.resetClicked();
 
-		GAME.board.board[tile[0]][tile[1]].selected = true;
-		UI.selectedTile = GAME.board.board[tile[0]][tile[1]];
+		BOARD.board[tile[0]][tile[1]].selected = true;
+		UI.selectedTile = BOARD.board[tile[0]][tile[1]];
 
-		console.log('Tile', GAME.board.board[tile[0]][tile[1]].center[2], GAME.board.board[tile[0]][tile[1]].center[3], 'selected.');
+		console.log('Tile', BOARD.board[tile[0]][tile[1]].center[2], BOARD.board[tile[0]][tile[1]].center[3], 'selected.');
 
 		drawBoardState();
 
@@ -535,31 +611,81 @@ function testClick (ev) {
 	}
 }
 
-function placeCamel (ev) {
-
-	if (GAME.turn.camels < 2) {
-		var tile = UI.selectedTile;
-		var act_pl = GAME.turn.activeplayer;
-
-		GAME.turn.camels++
-		tile.camel = new Camel(act_pl);
-
-		drawBoardState();
-		console.log('Camel placed!', UI.selectedTile)
-	} else {
-		window.alert('No more camels!');
-	}
+function playPiece (ev) {
 	
+	var buttons = document.querySelector('div#pieceColor').children;
+	var color = '';
+
+	for (var i = 0; i < buttons.length; i++) {
+		if (buttons[i].checked === true) {
+			color = buttons[i].value;
+		};
+	};
+
+	var x = UI.selectedTile.center[2];
+	var y = UI.selectedTile.center[3];
+	var adjacent = BOARD.getAdjacent([x,y]);
+	console.log(x, y, adjacent);
+	var legal = false;
+	
+	
+	for (var j = 0; j < adjacent.length; j++) {		
+		if (adjacent[j].piece.color === color) {
+			legal = true;
+		};
+	};
+
+	if (typeof UI.selectedTile != 'undefined' && UI.selectedTile.piece.exists === false && legal === true) {
+
+		UI.selectedTile.piece.exists = true;
+		UI.selectedTile.piece.color = color;
+		drawBoardState();
+		console.log('Piece Played!');
+
+	}
 }
 
-function newTurn (ev) {
-	var player = (GAME.turn.activeplayer + 1) % 2;
-	var turn = GAME.turn.turn + 1;
-	GAME.turn = new TurnState(player, turn);
-	updateUI();
+function placeLeader (ev) {
+	
+	var buttons = document.querySelector('div#pieceColor').children;
+	var color = '';
+
+	for (var i = 0; i < buttons.length; i++) {
+		if (buttons[i].checked === true) {
+			color = buttons[i].value;
+		};
+	};
+
+	var legal = true;
+
+	for (var i = 0; i < FLAGS.LeadersPlaced.length; i++) {
+		if (FLAGS.LeadersPlaced[i] === color) {
+			var legal = false
+		};
+	};
+
+	if (typeof UI.selectedTile != 'undefined' && UI.selectedTile.piece.exists === false && legal === true && color.length > 2) {
+
+		FLAGS.LeadersPlaced.push(color);
+		UI.selectedTile.piece.exists = true;
+		UI.selectedTile.piece.color = color;
+		drawBoardState();
+		console.log('Piece Played!');
+
+	}
 }
 
+function removePiece (ev) {
 
+	if (typeof UI.selectedTile != 'undefined' && UI.selectedTile.piece.exists === true) {
+
+		UI.selectedTile.piece.exists = false;
+		UI.selectedTile.piece.color = false;
+		drawBoardState();
+		console.log('Piece Removed');
+
+	}
+}
 
 document.body.onload = prepareBoard;
 document.querySelector('canvas').addEventListener('click', testClick);
